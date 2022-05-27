010c456c70831334a7f5f4cd8e3fec5506b9efc7
sjeffry-o <kronizzyd@gmail.com>
2022-05-27 15:31:56 +0500

    supports CORS, search function returns wrapped ids

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
-from flask import request, Flask, Response
+from flask import request, Flask
+from vectorizeSearch import vectorizeSearch
+import ngtpy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+index = ngtpy.Index('test')
 
 @app.route('/search')
 def data():
     query = request.args.get('query')
-    resp = Response(query)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+    result = vectorizeSearch(query, index)
+    result.headers['Access-Control-Allow-Origin'] = '*'
+    return result
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3001, host='0.0.0.0')
--- backend/vectorizeSearch.py
+++ backend/vectorizeSearch.py
@@ -1,18 +1,22 @@
 import ruclip
 import ngtpy
+from flask import jsonify
 
 device = 'cpu'
 model, processor = ruclip.load('ruclip-vit-base-patch16-224', device=device)
 
 def vectorizeSearch(new_text, index): 
     input_ids = processor.encode_text(new_text).unsqueeze(0)
-    print(input_ids.shape, processor.decode_text(input_ids))
+    #print(input_ids.shape, processor.decode_text(input_ids))
     embedding = model.encode_text(input_ids).detach().numpy()
     results = index.search(embedding, 6)
+    ids = []
     for i, (id, distance) in enumerate(results):
-        print(str(i) + ": " + str(id) + ", " + str(distance))
-        object = index.get_object(id)
-        print(object)
+        ids.append(id)
+        #print(str(i) + ": " + str(id) + ", " + str(distance))
+        #object = index.get_object(id)
+        #print(object)
+    return jsonify(ids)
 
 if __name__ == "__main__":
     index = ngtpy.Index('test')

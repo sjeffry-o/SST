08b3ef1135920707334d5419c0f0bfdfa48d2e8a
sjeffry-o <kronizzyd@gmail.com>
2022-06-24 15:38:19 +0500

    dumping idxs of lost text embeddings; testing data

--- backend/computeVectors.py
+++ backend/computeVectors.py
@@ -1,10 +1,12 @@
-from glob import glob
 import gc
 import torch
 import ruclip
 import pickle
 from PIL import Image
 from torchvision.transforms import Compose, CenterCrop, ToTensor, Normalize, Resize
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 device = 'cuda'
 model, processor = ruclip.load('ruclip-vit-base-patch16-224', device=device)
@@ -19,9 +21,8 @@ img_transform = Compose([
 
 vis_model = model.visual.cuda().float().eval()
 
-def computeImgVectors(glob_path, save=True):
+def computeImgVectors(img_paths, save=True):
     img_vectors = torch.Tensor().to('cpu')
-    img_paths = glob(glob_path)
     device = 'cuda'
     count = 0
 
@@ -29,9 +30,6 @@ def computeImgVectors(glob_path, save=True):
       for path in img_paths:
         if count % 20000 == 0:
           print(count, "passed")
-          if save:
-            with open(f'img_embeds{count}.pkl', 'wb') as f:
-              pickle.dump(img_vectors, f)
         image = Image.open(path)
         image = image.convert("RGB")
         image = img_transform(image)
@@ -39,22 +37,33 @@ def computeImgVectors(glob_path, save=True):
         image = image.to(device)
         img_vectors = torch.cat([img_vectors, vis_model(image.unsqueeze(0)).to('cpu')])
         count += 1
+    if save:
+        with open('img_embeds.pkl', 'wb') as f:
+            pickle.dump(img_vectors, f)
     return img_vectors
 
 def computeTextVectors(text_list, save=True):
     text_vectors = torch.Tensor().to('cpu')
+    lost_idxs = []
     device = 'cuda'
     count = 0
+    last_shape = 0
 
     with torch.no_grad():
       for text in text_list:
+        last_shape = text_vectors.shape[0]
+        text = text[0]
         if count % 20000 == 0:
           print(count, "passed")
-          if save:
-            with open(f'text_embeds{count}.pkl', 'wb') as f:
-              pickle.dump(text_vectors, f)
         input_ids = processor.encode_text(text).unsqueeze(0).cuda()
         embedding = model.encode_text(input_ids)
         text_vectors = torch.cat([text_vectors, embedding.to('cpu')])
+        if text_vectors.shape[0] == last_shape:
+            lost_idxs.append(count)
         count += 1
+    if save:
+        with open('text_embeds.pkl', 'wb') as f:
+            pickle.dump(text_vectors, f)
+        with open('lost_idxs.pkl', 'wb') as f:
+            pickle.dump(lost_idxs, f)
     return text_vectors
--- backend/createIndex.py
+++ backend/createIndex.py
@@ -1,14 +1,16 @@
 import ngtpy
+import pickle
+import torch
 from computeVectors import computeImgVectors, computeTextVectors
- 
+
 def indexCreateVerify(vectors, indexName):
     dim = vectors.shape[1]
-     
+
     ngtpy.create(indexName, dim)
     index = ngtpy.Index(indexName)
     index.batch_insert(vectors)
     index.save()
-     
+
     query = vectors[0]
     results = index.search(query, 10)
     for i, (id, distance) in enumerate(results) :
@@ -16,18 +18,25 @@ def indexCreateVerify(vectors, indexName):
         object = index.get_object(id)
         #print(object)
 
-def computeMeanJointEmbeddings(im_glob, texts):
-    img_vectors = computeImgVectors(im_glob)
+def computeMeanJointEmbeddings(im_paths, texts, save=True):
+    img_vectors = computeImgVectors(im_paths)
     text_vectors = computeTextVectors(texts)
     vectors_mean = torch.mean(torch.stack([img_vectors, text_vectors]), dim=0)
+    if save:
+        with open('joint_embeds.pkl', 'wb') as f:
+            pickle.dump(vectors_mean, f)
     return vectors_mean
 
 if __name__ == "__main__":
-    import glob
-    import torch
-    im_glob = './static/*'
-    im_paths = glob.glob(im_glob)
-    texts = ['test'] * len(im_paths)
-    vectors_mean = computeMeanJointEmbeddings(im_glob, texts)
+    with open('./imgNameMatch.pkl', 'rb') as f:
+        imgToName = pickle.load(f)
+    im_paths = list(imgToName.keys())
+    texts = list(imgToName.values())
+    assert [''] not in texts
+    assert len(im_paths) == len(texts)
+    assert all([len(name[0]) > 0 for name in texts])
+    assert all([len(name) == 1 for name in texts])
+    print(len(im_paths), len(texts))
+    vectors_mean = computeMeanJointEmbeddings(im_paths, texts)
     print(vectors_mean.shape)
-    indexCreateVerify(vectors_mean, 'test')
+    indexCreateVerify(vectors_mean, 'opendataJointSearchIndex')
